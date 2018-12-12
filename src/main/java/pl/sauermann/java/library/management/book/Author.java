9633bbf46822483da91738ac1da1c67e336517e8
dawidsau <dawidsau@gmail.com>
2018-12-12 19:00:20 +0100

    Book and Author class modification

--- src/main/java/pl/sauermann/java/library/management/book/Author.java
+++ src/main/java/pl/sauermann/java/library/management/book/Author.java
@@ -1,4 +1,62 @@
 package pl.sauermann.java.library.management.book;
 
-public class Author {
+import java.util.List;
+import java.util.Objects;
+
+public class Author implements Comparable<Author> {
+
+    private String name;
+    private String surname;
+    private List<Book> bookList;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Author(String name, String surname, List<Book> bookList) {
+        this.name = name;
+        this.surname = surname;
+        this.bookList = bookList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        Author author = (Author) other;
+
+        return name.equals(author.name) &&
+                surname.equals(author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname());
+    }
+
+    @Override
+    public String toString() {
+        return "Author:" + name + " " + surname;
+    }
+
+    @Override
+    public int compareTo(Author o) {
+        return surname.equals(o.surname) ?
+                name.compareToIgnoreCase(o.name) :
+                surname.compareToIgnoreCase(o.surname);
+    }
 }
--- src/main/java/pl/sauermann/java/library/management/book/Book.java
+++ src/main/java/pl/sauermann/java/library/management/book/Book.java
@@ -1,4 +1,87 @@
 package pl.sauermann.java.library.management.book;
 
-public class Book {
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Book implements Comparable<Book>{
+
+    private String title;
+    private Author author;
+    private long numberOfPages;
+    private BookType bookType;
+    private LocalDate distributionDate;
+
+    public Book(String title, Author author, BookType bookType) {
+        this.title = title;
+        this.author = author;
+        this.bookType = bookType;
+    }
+
+    public Book(String title, Author author, long numberOfPages, BookType bookType) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.bookType = bookType;
+    }
+
+    public Book(String title, Author author, long numberOfPages, LocalDate distributionDate, BookType bookType) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.distributionDate = distributionDate;
+        this.bookType = bookType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public long getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public LocalDate getDistributionDate() {
+        return distributionDate;
+    }
+
+    public BookType getBookType() {
+        return this.bookType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return title.equals(book.title) &&
+                author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getAuthor());
+    }
+
+    @Override
+    public String toString() {
+        return "Book: " + title + " " + author;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return title.equals(o.title) ?
+                author.getSurname().compareToIgnoreCase(o.author.getSurname()) :
+                title.compareToIgnoreCase(o.title) ;
+    }
+
+    enum BookType {
+        FICTION, ACTION, COOKBOOK, CRIME,
+        DRAMA, GUIDE, FANTASY, HISTORY;
+    }
+
 }
